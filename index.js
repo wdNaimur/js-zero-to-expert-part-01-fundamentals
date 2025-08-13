6be814058f33f9918bff9ddb35fce2c3ba692c26
NaimurWD <wd.naimur@gmail.com>
2025-08-13 10:16:42 -0700

    Bmi calculator practice.

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const currentYear = new Date().getFullYear();
 const birthYear = 2001;
 const ageNaimur = currentYear - birthYear;
 console.log("your age is", age);
+
+// Constant values
+const weight = 70;
+const height = 1.75;
+
+// BMI formula: weight / (height * height)
+const bmi = weight / (height * height);
+
+console.log("Your BMI is:", bmi);
